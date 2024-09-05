bdf23bd794dcaf419ff9ac5187c8de98e2c36b20
Killakhi <124313338+Killakhi@users.noreply.github.com>
2024-09-05 02:30:43 -0400

    Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] float jumpForce = 10f; //Gets the float for the jumps power
+    public Rigidbody2D rb; //Creates a public for the object's rigidbody
+    public bool isjumping;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      rb = GetComponent<Rigidbody2D>(); //Gets the rigidbody
+      isjumping = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jump(); //Calls the function 
+    }
+
+    public void Jump()
+    {   
+        //If space is pressed, this happens 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse); //Adds force to the vector in a up direction
+            isjumping=true;
+        }
+        else
+        {
+            isjumping = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Walls.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walls : MonoBehaviour
+{
+    [SerializeField] float speed = 2f;
+    public Transform player; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
